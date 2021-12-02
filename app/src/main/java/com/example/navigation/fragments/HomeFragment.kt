803c7af36ed90e323bdf73a1cf83fd1a2ff604f8
Barbare Gogrichiani <barbare.gogritchiani.1@btu.edu.ge>
2021-12-02 19:48:45 +0400

    Initial commit

---
+++ app/src/main/java/com/example/navigation/fragments/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.example.navigation.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.navigation.R
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var editTextAmount : EditText
+    private lateinit var sendButton : Button
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        editTextAmount = view.findViewById(R.id.editTextAmount)
+        sendButton = view.findViewById(R.id.buttonSend)
+
+        val controller = Navigation.findNavController(view)
+
+        sendButton.setOnClickListener {
+            val amountText = editTextAmount.text.toString()
+
+            if (amountText.isEmpty())
+                return@setOnClickListener
+            val amount = amountText.toInt()
+            val action = HomeFragmentDirections.actionHomeFragmentToDashboardFragment2(amount)
+            controller.navigate(action)
+
+        }
+    }
+
+
+
+
+
+}
